6c732a39545e25788f6ef97204e300d42ca142c2
Ryan Santos <ryansantos86@gmail.com>
2019-07-15 18:48:40 -0700

    cleanup

--- src/index.js
+++ src/index.js
@@ -1,9 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import AOS from 'aos';
-import 'aos/dist/aos.css';
 
 import App from './App';
 
-AOS.init({ duration: 800, easing: 'ease' });
 ReactDOM.render(<App />, document.getElementById('root'));
